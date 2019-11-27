bbf1440cc874cda3b19c8c0fe42bc14cd1be1e1a
Gaida Escobar <gescobar129@gmail.com>
2019-11-27 10:17:11 -0500

    completed up to checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,43 @@
 import React from "react";
+import BotContainer from './BotCollection';
+import YourBotArmy from './YourBotArmy';
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  
+  state ={
+    bots: [],
+    myArmy: []
+  }
+
+  addToArmy = (bot) => {
+    if (!this.state.myArmy.includes(bot)) {
+      this.setState({
+        myArmy: [...this.state.myArmy, bot]
+      })
+    }
+  }
+
+  removeFromArmy = (bot) => {
+    this.setState({
+      myArmy: this.state.myArmy.filter(element => element !== bot)
+    })
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        bots: data
+      })
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <BotContainer bots={this.state.bots} addToArmy={this.addToArmy}/>
+        <YourBotArmy bots={this.state.myArmy} removeFromArmy={this.removeFromArmy}/>
       </div>
     );
   }
