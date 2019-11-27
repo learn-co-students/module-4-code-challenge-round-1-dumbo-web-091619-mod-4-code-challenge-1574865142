06a9695b9361e82967bb02440c4acc482d510c03
Lyle Dickie <lyle.e.dickie@gmail.com>
2019-11-27 11:04:26 -0500

    Unable to finish challenge

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -20,7 +20,8 @@ class BotCollection extends React.Component {
   render(){
 
     console.log(this.state.bots)
-    // I can get the data but I can't do anything with it. when i try to map it says that the data is null and can't be mapped. I also can't set the data to state and access it that way as I also get null. Can't get on the problem.
+    console.log(this.props.botsData)
+    // I can get the data but I can't do anything with it. when i try to map it says that the data is null and can't be mapped. I also can't set the data to state and access it that way as I also get null. Can't get on the problem, I don't really know what I'm doing here.
 
   	return (
   	  <div className="ui four column grid">
