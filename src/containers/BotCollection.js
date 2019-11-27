7b70adf3607b9e656d9520a1d2513c458bcc8200
awazienommy <coawazie@gmail.com>
2019-11-27 11:15:38 -0500

    done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,13 +1,23 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
+
 class BotCollection extends React.Component {
   //your code here
 
+  
+  
+
   render(){
+	  console.log(this.props.allBots)
+	  let allBots= this.props.map
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
+				{/* {allBots.map( bot => {
+					// <BotCard bot={this.bot} />
+				})} */}
+				{/* <BotCard /> */}
     		  {/*...and here..*/}
     		  Collection of all bots
     		</div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,36 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+import axios from 'axios'
+
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  constructor(props) {
+	  super(props)
+  
+	  this.state = {
+		   allBots: []
+	  }
+  }
+
+
+  componentDidMount(){
+	  axios.get('https://bot-battler-api.herokuapp.com/api/v1/bots')
+	  .then(r =>{
+		this.setState({
+			allBots: r.data
+		})
+	  })
+  }
+
   render() {
+    // console.log(this.state.allBots)
     return (
       <div>
+        <YourBotArmy />
+        <BotCollection allBots={this.state.allBots}/>
         {/* put your components here */}
       </div>
     );
