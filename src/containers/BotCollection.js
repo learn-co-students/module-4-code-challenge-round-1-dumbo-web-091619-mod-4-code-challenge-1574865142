e1bf10d5099ff23c7e470b1eab7de9702a75d4cd
Jamal <moulayjam@gmail.com>
2019-11-27 11:28:47 -0500

    dsfhksjdfhsd

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,18 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+
 
   render(){
+	let  allBots = this.props.bot
+	console.log(allBots)
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
     		  {/*...and here..*/}
     		  Collection of all bots
+			  {/* <BotCard /> */}
     		</div>
   	  </div>
   	);
